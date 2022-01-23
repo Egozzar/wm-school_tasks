9b14afacc3bd97228609b7337ffd95f0d9fdc9f1
Egozzar <Egozzar@yandex.ru>
2022-01-23 16:55:39 +0300

    Двоеточие между нечётными цифрами

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+const app = Vue.createApp({
+	data() {
+		return {
+			string: '',
+			results: [],
+		}
+	},
+	methods: {
+		colonOdd () {
+			let arr = this.string.split('');
+
+			for (let i = 0; i < arr.length; i++) {
+				if ((arr[i] % 2) && (arr[i + 1] % 2)) {
+					arr.splice(i + 1, 0, ':');
+					i++;
+				}
+			}
+
+			this.results.push( arr.join('') );
+			this.string = '';
+		}
+	}
+});
+
+const vm = app.mount('#app');
