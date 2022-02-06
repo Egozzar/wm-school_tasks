afbb5ed7eb3acaa2b6be489db831ac718142b421
Egozzar <Egozzar@yandex.ru>
2022-02-06 18:43:59 +0300

    Объединить два массива без дублирования элементов

--- script.js
+++ script.js
@@ -8,39 +8,14 @@ const app = Vue.createApp({
 		}
 	},
 	methods: {
-		expand() {
-			let arr = this.string.split('')
-				.map(item => Array.from({0: +item,
-					1: Array.from({0: ++item, length:1}), length:2}));
+		concatOrigin() {
+			let [arr1, arr2] = this.string.split(' ')
+				.map(item => item.split('').map(item => +item));
 
-			let start = this.copyDeep(arr);
-			let end = this.recurs(arr);
-			this.results.push({start, end});
+			let end = new Set([...arr1, ...arr2]);
+			this.results.push({start: `[${arr1}] + [${arr2}]`, end: [...end]});
 			this.string = '';
 		},
-		copyDeep(arr) {
-			let clone = [];
-			arr.forEach((item, ind) => {
-				if (Array.isArray(item)) {
-					clone[ind] = this.copyDeep(item);
-				} else {
-					clone[ind] = item;
-				}
-			});
-			return clone;
-		},
-		recurs(arr) {
-			let clone = [];
-
-			for (let i = 0; i < arr.length; i++) {
-				if (arr[i].fill) {
-					clone = clone.concat( this.recurs(arr[i]) );
-				} else {
-					clone.push(arr[i]);
-				}
-			}
-			return clone;
-		}
 	}
 });
 
