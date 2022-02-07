2cbda12d4efefda530b3c1a95caeb825bd403224
Egozzar <Egozzar@yandex.ru>
2022-02-07 14:03:58 +0300

    Сумма и произведение значений массива

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const app = Vue.createApp({
 		}
 	},
 	methods: {
-		sumOfSquares() {
-			let arr = this.string.split('');
-			let start = arr.map(item => +item);
+		sumMul() {
+			let arr = this.string.split('').map(item => +item);
 
-			let end = arr.reduce((sum, val) => {
-				return sum + val * val;
-			}, 0);
-			this.results.push({start, end});
+			let start = arr.slice();
+			let sum = arr.reduce((sum, val) => sum + val, 0);
+			let mul = arr.length ? arr.reduce((mul, val) => mul * val) : 0;
+
+			this.results.push({start, end: `sum = ${sum}, mul = ${mul}`});
 			this.string = '';
 		},
 	}
