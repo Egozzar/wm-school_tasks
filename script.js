628d113c8ff32d13c349ff5f501ed5cbafa0dccf
Egozzar <Egozzar@yandex.ru>
2022-02-08 14:12:08 +0300

    Удалить определенный элемент из массива

--- script.js
+++ script.js
@@ -4,31 +4,20 @@ const app = Vue.createApp({
 	data() {
 		return {
 			string: '',
-			array: [],
-			result: '',
+			results: [],
 		}
 	},
 	methods: {
-		startingSubstring(field) {
-			let [str1, str2] = this.array;
-			let res = '';
+		removeItem() {
+			let [str, elem] = this.string.split(' ');
+			let arr = str.split('');
+			let start = arr.slice();
 
-			for (let i = 0; i < str1.length; i++) {
-				if (str1[i] === str2[i]) {
-					res += str1[i];
-				} else break;
-			}
-			this.result = res === '' ? 'Совпадений не найдено' : res;
-		},
-		creating() {
-			if (this.array.length === 2) {
-				alert('Массив уже создан. Нажмите кнопку Search.');
-				this.string = '';
-				return;
-			}
-			this.array.push(this.string);
+			let end = arr.filter(item => item !== elem);
+
+			this.results.push({start, end});
 			this.string = '';
-		}
+		},
 	}
 });
 
