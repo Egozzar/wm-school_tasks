33407a8da8a61bbd9bf4debfcfda0e6b7bf2220b
Egozzar <Egozzar@yandex.ru>
2022-02-08 20:41:23 +0300

    Переместить элемент массива из одной позиции в другую

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const app = Vue.createApp({
 	data() {
 		return {
 			string: '',
+			start: [22, true, 'Жираф', 157, null, 'Crow', [1, 2, 3]],
 			results: [],
 		}
 	},
 	methods: {
-		generateNumbers() {
-			let [elem, length] = this.string.split(' ');
-			elem = +elem;
-			let start = `Начальный элемент: ${elem}, количество: ${+length}шт `;
+		moveElement() {
+			let [pos, fut] = this.string.split(',').map(item => +item);
 
-			let end = Array.from({length}, () => elem++);
+			if (pos < 0 || pos > this.start.length - 1 ||
+				  fut < 0 || fut > this.start.length - 1) {
+				alert('Значения позиций должны быть в диапазоне \nпрепарируемого массива, т.е. 0 >= позиция <= 6');
+				this.string = '';
+				return;
+			}
 
-			this.results.push({start, end});
+			let clone = this.start.slice();
+			let elem = clone.splice(pos, 1)[0];
+			clone.splice(fut, 0, elem);
+
+			this.results.push({exp: `Элемент ${elem} с позиции ${pos} на позицию ${fut}`,
+			                   res: clone});
 			this.string = '';
 		},
 	}
